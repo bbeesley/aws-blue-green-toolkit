854b825d2c909fbb6dadb8a58cc97c41270a3453
Bill Beesley <bill.beesley@dazn.com>
2019-12-24 22:02:38 +0000

    feat(sqs-tools): add tools for purging SQS queues and DLQs

---
+++ src/main/SqsTools.ts
@@ -0,0 +1,71 @@
+import { AwsConfig } from './common-interfaces';
+import { SQS } from 'aws-sdk';
+import { StackReference } from './constants';
+
+/**
+ * Configuration options for the SQS toolkit
+ * @export
+ * @interface SqsConfig
+ * @extends {AwsConfig}
+ */
+export interface SqsConfig extends AwsConfig {
+  queueA: SqsQueue;
+  queueB: SqsQueue;
+}
+
+interface SqsQueue {
+  queueName: string;
+  dlqName?: string;
+}
+
+/**
+ * Toolkit for SQS operations
+ * @export
+ * @class SqsTools
+ */
+export class SqsTools {
+  config: SqsConfig;
+  sqs: SQS;
+
+  /**
+   * Creates an instance of SqsTools.
+   * @param {SqsConfig} config
+   * @memberof SqsTools
+   */
+  constructor(config: SqsConfig) {
+    this.config = config;
+    this.sqs = new SQS({ region: this.config.awsRegion });
+  }
+
+  /**
+   * Gets queue data based on a stack reference
+   * @private
+   * @param {StackReference} ref - Reference to a subscription queue stack
+   * @returns {SqsQueue}
+   * @memberof SqsTools
+   */
+  private getQueue(ref: StackReference): SqsQueue {
+    return ref === StackReference.a ? this.config.queueA : this.config.queueB;
+  }
+
+  /**
+   * Purges a queue pair (q and dlq) based on config and queue reference
+   * @param {StackReference} reference - Reference to a subscription queue stack
+   * @returns {Promise<void>}
+   * @memberof SqsTools
+   */
+  public async purgeQueues(reference: StackReference): Promise<void> {
+    const queue = this.getQueue(reference);
+    await Promise.all(
+      Object.values(queue).map(async queueName => {
+        if (queueName) {
+          await this.sqs
+            .purgeQueue({
+              QueueUrl: `https://sqs.${this.config.awsRegion}.amazonaws.com/${this.config.awsProfile}/${queueName}`,
+            })
+            .promise();
+        }
+      })
+    );
+  }
+}
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,3 +1,4 @@
 export * from './constants';
 export { AuroraTools } from './AuroraTools';
 export { SnsTools } from './SnsTools';
+export { SqsTools } from './SqsTools';
