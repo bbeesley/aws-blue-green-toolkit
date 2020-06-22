9789c36a561ae32cb7890796046ad0a94b7866de
Bill Beesley <bill.beesley@dazn.com>
2020-06-22 14:31:26 +0100

    build(commitlint): disable new body max length rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,9 @@
+// eslint-disable-next-line node/no-unpublished-require
 const { types } = require('conventional-commit-types');
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: { 'type-enum': [2, 'always', Object.keys(types)] },
+  rules: {
+    'type-enum': [2, 'always', Object.keys(types)],
+    'body-max-line-length': [0, 'always', 100],
+  },
 };
