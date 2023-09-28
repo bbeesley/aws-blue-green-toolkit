09ad3c1fabe099ba07d669e28fe7bfd7ffc5766a
Bill Beesley <bill.beesley@soundcloud.com>
2023-09-28 12:34:51 +0100

    feat(aurora-tools): look up tags on cluster if none supplied in config
    Adds functionality to allow using tagging functions in a more generic way. If the tags object passed
    into the constructor is empty, the applyTags method will now look up the tags on the cluster and use
    them as the base to apply to the newly created instance. Additionally, you can now set namespace to
    '*' to disable filtering tagging operations based on a namespace.

--- src/main/@types/aurora-tools.ts
+++ src/main/@types/aurora-tools.ts
@@ -8,9 +8,34 @@ import type { AwsConfig } from './common.js';
  * @extends {AwsConfig}
  */
 export type AuroraConfig = {
+  /**
+   * The name of a cluster
+   *
+   * @type {string}
+   */
   clusterNameA: string;
+  /**
+   * The name of a secondary cluster (just set to an empty string if not using cluster pairs)
+   *
+   * @type {string}
+   */
   clusterNameB: string;
+  /**
+   * The minimum size, below which the cluster should not scale in
+   *
+   * @type {number}
+   */
   minimumClusterSize: number;
+  /**
+   * Whether or not we should skip delete snapshots
+   *
+   * @type {boolean}
+   */
   skipDeleteSnapshots: boolean;
+  /**
+   * Common namespace for the clusters. Use '*' to ignore filtering operations by namespace
+   *
+   * @type {string}
+   */
   namespace: string;
 } & AwsConfig;
